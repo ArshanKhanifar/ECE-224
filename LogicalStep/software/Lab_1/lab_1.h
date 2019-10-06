cd80b241de120c87abb854e442651076fc947621
Arshan Khanifar <akhanifa@uwaterloo.ca>
2019-10-05 22:32:38 -0400

    add lab1 stuff

---
+++ LogicalStep/software/Lab_1/lab_1.h
@@ -0,0 +1,22 @@
+/*
+ * lab_1.h
+ *
+ *  Created on: Sep 25, 2019
+ *      Author: akhanifa
+ */
+
+#ifndef LAB_1_H_
+#define LAB_1_H_
+
+
+#include "system.h"
+#include "alt_types.h"
+#include <stdio.h>
+#include <unistd.h>
+#include "system.h"
+#include "priv/alt_legacy_irq.h"
+#include "sys/alt_irq.h"
+#include "altera_avalon_pio_regs.h"
+
+
+#endif /* LAB_1_H_ */
---
+++ LogicalStep/software/Lab_1/obj/default/hello_world.d
@@ -0,0 +1,6 @@
+obj/default/hello_world.o: hello_world.c ../Lab_1_bsp/system.h \
+ ../Lab_1_bsp/linker.h
+
+../Lab_1_bsp/system.h:
+
+../Lab_1_bsp/linker.h:
